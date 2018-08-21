9be252b08953db66483a518828f284e25a22ec88
Melanie Mendavia <mlmendavia@gmail.com>
2018-08-21 21:50:39 +0000

    automatically backed up by learn

--- lib/while.rb
+++ lib/while.rb
@@ -1,17 +1,11 @@
 
 #
-# counter = 0
-# def using_while
-#   loop do
-#     counter += 1
-#     puts "Wingardium Leviosa"
-# end
-# end
-
-
-
 counter = 0
-while counter < 10
-  puts "Wingaridum Leviosa"
-  counter += 1
+def using_while
+  loop do
+    counter += 1
+    puts "Wingardium Leviosa"
 end
+end
+
+
